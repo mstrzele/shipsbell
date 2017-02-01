cde83e455ce7d10b9ceb4481c08a06f59b31f198
Maciej Strzelecki <maciej.strzelecki@gmail.com>
2017-02-01 22:57:02 +0100

    Increase version

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ import (
 var metadata string
 
 func version() (string, error) {
-	v, err := semver.NewVersion("0.1.0")
+	v, err := semver.NewVersion("0.2.0")
 	if err != nil {
 		return v.String(), err
 	}
